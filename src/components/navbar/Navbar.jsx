64c52a285dcd01aaa03a8878c3da1183f3bbf1c2
gomaman <999tmll@gmail.com>
2023-07-13 13:42:57 +0300

    blog/article

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Menu = () => (
       <a href="#possibility">Open AI</a>
     </p>
     <p>
-      <a href="#ftr">Case Studies</a>
+      <a href="#future">Case Studies</a>
     </p>
     <p>
       <a href="#blog">Library</a>
